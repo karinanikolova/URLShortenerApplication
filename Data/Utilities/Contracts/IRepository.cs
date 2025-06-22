530a53b4031264fdcd9297dcef2b9d72f6692ea7
karinanikolova <lianna_kn@yahoo.com>
2025-06-22 21:06:21 +0300

    Created the IRepository interface.

---
+++ Data/Utilities/Contracts/IRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace URLShortenerApp.Data.Utilities.Contracts
+{
+	/// <summary>
+	///  Providing a single database context for use across multiple services, thus making future database provider changes easier over time, if necessary.
+	/// </summary>
+	public interface IRepository
+	{
+		IQueryable<TEntity> All<TEntity>() where TEntity : class;
+
+		IQueryable<TEntity> AllReadOnly<TEntity>() where TEntity : class;
+
+		Task AddAsync<TEntity>(TEntity entity) where TEntity : class;
+
+		void Add<TEntity>(TEntity entity) where TEntity : class;
+
+		Task<int> SaveChangesAsync();
+
+		int SaveChanges();
+
+		void Delete<TEntity>(TEntity entityToDelete) where TEntity : class;
+
+		void DeleteRange<TEntity>(params TEntity[] entitiesToDelete) where TEntity : class;
+
+		Task<TEntity?> GetByIdAsync<TEntity>(object id) where TEntity : class;
+	}
+}
