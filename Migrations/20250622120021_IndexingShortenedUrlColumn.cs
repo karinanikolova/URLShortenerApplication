212b08f02587c9fb7eb6809871bdc0daf3cb088e
karinanikolova <lianna_kn@yahoo.com>
2025-06-22 20:44:13 +0300

    Creating new migration called IndexingShortenedUrlColumn for indexing ShortenedUrl in the URLs table.

---
+++ Migrations/20250622120021_IndexingShortenedUrlColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace URLShortenerApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class IndexingShortenedUrlColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_URLs_ShortenedUrl",
+                table: "URLs",
+                column: "ShortenedUrl",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_URLs_ShortenedUrl",
+                table: "URLs");
+        }
+    }
+}
