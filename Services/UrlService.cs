90f1e365ba5a188ddff26edc74adf4355e666ce0
karinanikolova <lianna_kn@yahoo.com>
2025-06-22 21:08:36 +0300

    Created UrlService implementing IUrlService.

---
+++ Services/UrlService.cs
@@ -0,0 +1,36 @@
+﻿using URLShortenerApp.Data.Models;
+using URLShortenerApp.Data.Utilities.Contracts;
+using URLShortenerApp.Services.Contracts;
+
+namespace URLShortenerApp.Services
+{
+	public class UrlService : IUrlService
+	{
+		private readonly IRepository _repository;
+
+		public UrlService(IRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task AddUrl(string url)
+		{
+			var urlEntity = new URL()
+			{
+				OriginalUrl = url,
+				ShortenedUrl = GenerateShortenedUrl(url),
+				CreationDate = DateTime.UtcNow
+			};
+
+			await _repository.AddAsync<URL>(urlEntity);
+			await _repository.SaveChangesAsync();
+		}
+
+		private string GenerateShortenedUrl(string longUrl)
+		{
+			//To be implemented
+
+			return "";
+		}
+	}
+}
