e9cb926052adc5c2c0dad6786f559bbdf8ad7bb1
karinanikolova <lianna_kn@yahoo.com>
2025-06-22 20:51:11 +0300

    Updated AppDbContext extension method and implemented AddAppServices extension method in ServiceCollectionExtension.

--- Extensions/ServiceCollectionExtension.cs
+++ Extensions/ServiceCollectionExtension.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 using URLShortenerApp.Data;
+using URLShortenerApp.Data.Utilities;
+using URLShortenerApp.Data.Utilities.Contracts;
+using URLShortenerApp.Services;
+using URLShortenerApp.Services.Contracts;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
+	/// <summary>
+	/// Extension methods for IServiceCollection in order to keep the Program.cs file decluttered.
+	/// </summary>
 	public static class ServiceCollectionExtension
 	{
 		public static IServiceCollection AddAppDbContext(this IServiceCollection services, IConfiguration configuration)
@@ -10,7 +18,15 @@ namespace Microsoft.Extensions.DependencyInjection
 			services.AddDbContext<AppDbContext>(options =>
 				options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
-			//services.AddScoped<IRepository, Repository>();
+			services.AddScoped<IRepository, Repository>();
+
+			return services;
+		}
+
+		public static IServiceCollection AddAppServices(this IServiceCollection services)
+		{
+			// Adding custom services to the Inversion of Control container.
+			services.AddScoped<IUrlService, UrlService>();
 
 			return services;
 		}
