97fc07aee89ff580718145aa1dd60a8b5bdbcfa1
karinanikolova <lianna_kn@yahoo.com>
2025-06-22 21:09:11 +0300

    Created URLAddModel dto.

---
+++ Models/URL/URLAddModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using URLShortenerApp.Validation;
+using static URLShortenerApp.Constants.DataConstants.URL;
+using static URLShortenerApp.Constants.MessageConstants;
+
+namespace URLShortenerApp.Models.URL
+{
+	public class URLAddModel
+	{
+		[Required(ErrorMessage = RequiredFieldMessage)]
+		[StringLength(OriginalUrlMaxLength, MinimumLength = OriginalUrlMinLength, ErrorMessage = FieldLengthMessage)]
+		[ValidUrl]
+		public string Url { get; set; }
+
+		public DateTime CreationDate { get; set; }
+	}
+}
