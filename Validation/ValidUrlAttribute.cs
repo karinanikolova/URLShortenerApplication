83429938191748890181b928b578f165be9f17bd
karinanikolova <lianna_kn@yahoo.com>
2025-06-22 21:11:40 +0300

    Created custom ValidUrlAttribute for client-side validation.

---
+++ Validation/ValidUrlAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace URLShortenerApp.Validation
+{
+	public class ValidUrlAttribute : ValidationAttribute
+	{
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		{
+			if (value == null || string.IsNullOrWhiteSpace(value.ToString()))
+			{
+				return ValidationResult.Success;
+			}
+
+			var input = value.ToString().Trim();
+
+			if (!input.StartsWith("http://", StringComparison.OrdinalIgnoreCase) && !input.StartsWith("https://", StringComparison.OrdinalIgnoreCase))
+			{
+				input = "http://" + input;
+			}
+
+			if (Uri.TryCreate(input, UriKind.Absolute, out var uriResult) &&
+				(uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps))
+			{
+				return ValidationResult.Success;
+			}
+
+			return new ValidationResult("Please enter a valid URL.");
+		}
+	}
+}
