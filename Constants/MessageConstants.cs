ba66bee57f6677856bd14860611870619c6db09b
karinanikolova <lianna_kn@yahoo.com>
2025-06-22 20:46:20 +0300

    Created MessageConstants for holding validation messages.

---
+++ Constants/MessageConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace URLShortenerApp.Constants
+{
+	public static class MessageConstants
+	{
+		public const string FieldLengthMessage = "The field {0} must contain between {2} and {1} characters.";
+
+		public const string RequiredFieldMessage = "The field is required.";
+
+		public const string InvalidUrlMessage = "Please enter a valid URL.";
+	}
+}
