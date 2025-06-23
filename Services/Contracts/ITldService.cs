0e961129e9e487c2aba56ce4d263fcd1769e4682
karinanikolova <lianna_kn@yahoo.com>
2025-06-23 21:38:12 +0300

    Created ITldService interface.

---
+++ Services/Contracts/ITldService.cs
@@ -0,0 +1,12 @@
+﻿namespace URLShortenerApp.Services.Contracts
+{
+	/// <summary>
+	/// Interface for the TLD (Top-Level Domain) service.
+	/// </summary>
+	public interface ITldService
+	{
+		Task InitializeAsync();
+
+		bool IsValidTld(string tld);
+	}
+}
