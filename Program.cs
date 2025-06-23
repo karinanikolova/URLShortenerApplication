e76ed1c2f811d62efeed828a6d818ab19888d98a
karinanikolova <lianna_kn@yahoo.com>
2025-06-23 21:55:42 +0300

    Modified Program.cs by initializing the TldService and making the app run asynchronously.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
+using URLShortenerApp.Services.Contracts;
+
 namespace URLShortenerApp
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -13,8 +15,12 @@ namespace URLShortenerApp
 
 			var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+			var tldService = app.Services.GetRequiredService<ITldService>();
+			await tldService.InitializeAsync();
+
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -32,7 +38,7 @@ namespace URLShortenerApp
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
-            app.Run();
+            await app.RunAsync();
         }
     }
 }
