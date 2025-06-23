3d41cc56f2b640d602551c7822dc14d6ae924a92
karinanikolova <lianna_kn@yahoo.com>
2025-06-23 21:45:42 +0300

    Modified UrlService by implementing new methods.

--- Services/UrlService.cs
+++ Services/UrlService.cs
@@ -1,11 +1,17 @@
-﻿using URLShortenerApp.Data.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Numerics;
+using System.Text;
+using URLShortenerApp.Data.Models;
 using URLShortenerApp.Data.Utilities.Contracts;
+using URLShortenerApp.Models.URL;
 using URLShortenerApp.Services.Contracts;
 
 namespace URLShortenerApp.Services
 {
 	public class UrlService : IUrlService
 	{
+		private const string Base62Chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 		private readonly IRepository _repository;
 
 		public UrlService(IRepository repository)
@@ -13,12 +19,12 @@ namespace URLShortenerApp.Services
 			_repository = repository;
 		}
 
-		public async Task AddUrl(string url)
+		public async Task AddUrlAsync(string url)
 		{
 			var urlEntity = new URL()
 			{
 				OriginalUrl = url,
-				ShortenedUrl = GenerateShortenedUrl(url),
+				ShortenedUrl = await GenerateShortenedUrlAsync(url),
 				CreationDate = DateTime.UtcNow
 			};
 
@@ -26,11 +32,58 @@ namespace URLShortenerApp.Services
 			await _repository.SaveChangesAsync();
 		}
 
-		private string GenerateShortenedUrl(string longUrl)
-		{
-			//To be implemented
+		public async Task<URLViewModel> GetUrlViewModelByOriginalUrlAsync(string url) =>
+			await _repository.AllReadOnly<URL>()
+				.Where(u => u.OriginalUrl == url)
+				.Select(u => new URLViewModel()
+				{
+					OriginalUrl = u.OriginalUrl,
+					ShortenedUrl = u.ShortenedUrl
+				})
+				.FirstOrDefaultAsync();
 
-			return "";
+
+		public async Task<bool> OriginalUrlExistsAsync(string url) =>
+			await _repository.AllReadOnly<URL>()
+				.AnyAsync(u => u.OriginalUrl == url);
+
+		private async Task<string> GenerateShortenedUrlAsync(string longUrl, int length = 10)
+		{
+			var shortenedUrl = string.Empty;
+
+			do
+			{
+				// Generating a unique identifier for the URL and converting it to a byte array
+				byte[] guidBytes = Guid.NewGuid().ToByteArray();
+
+				// Converting the byte array to a BigInteger
+				var value = new BigInteger(guidBytes);
+
+				// Making sure the value is positive
+				if (value < 0)
+				{
+					value = -value;
+				}
+
+				// Generating the Base62 encoded string
+				StringBuilder base62Url = new StringBuilder();
+
+				while (value > 0)
+				{
+					value = BigInteger.DivRem(value, 62, out BigInteger remainder);
+
+					base62Url.Insert(0, Base62Chars[(int)remainder]);
+				}
+
+				// Taking the first N characters for the shortened URL
+				shortenedUrl = base62Url.ToString().Substring(0, length);
+
+			} while (await ShortenedUrlExistsAsync(shortenedUrl));
+
+			return shortenedUrl;
 		}
+		private async Task<bool> ShortenedUrlExistsAsync(string shortenedUrl) =>
+			await _repository.AllReadOnly<URL>()
+				.AnyAsync(u => u.ShortenedUrl == shortenedUrl);
 	}
 }
