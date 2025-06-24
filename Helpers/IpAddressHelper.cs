c3e26c040e59b81107be3b150d92c537184c1734
karinanikolova <lianna_kn@yahoo.com>
2025-06-24 22:21:15 +0300

    Created IpAddressHelper class with a method for retrieving current user's IP.

---
+++ Helpers/IpAddressHelper.cs
@@ -0,0 +1,24 @@
+﻿namespace URLShortenerApp.Helpers
+{
+	public static class IpAddressHelper
+	{
+		/// <summary>
+		/// Gets the IP address of the current request.
+		/// </summary>
+		/// <param name="context">The HTTP context.</param>
+		/// <returns>The IP address as a string.</returns>
+		public static string GetUserIpAddress(HttpContext context)
+		{
+			// Tries to get the user IP from the X-Forwarded-For (used by proxies), if available.
+			var forwardedHeader = context.Request.Headers["X-Forwarded-For"].FirstOrDefault();
+
+			if (!string.IsNullOrEmpty(forwardedHeader))
+			{
+				return forwardedHeader.Split(',').First().Trim();
+			}
+
+			// If the X-Forwarded-For header is not present, falls back to the RemoteIpAddress.
+			return context.Connection.RemoteIpAddress?.ToString() ?? "Unknown";
+		}
+	}
+}
