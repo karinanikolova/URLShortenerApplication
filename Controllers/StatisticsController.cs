8c3dc875d2fe092ef6ad0fdd34e242af5c760f97
karinanikolova <lianna_kn@yahoo.com>
2025-06-24 22:20:16 +0300

    Created StatisticsController for holding statistics page action methods.

---
+++ Controllers/StatisticsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using URLShortenerApp.Helpers;
+using URLShortenerApp.Services.Contracts;
+
+namespace URLShortenerApp.Controllers
+{
+	public class StatisticsController : Controller
+	{
+		private readonly IUrlService _urlService;
+
+		public StatisticsController(IUrlService urlService)
+		{
+			_urlService = urlService;
+		}
+
+		[HttpGet("/{shortenedUrl:shortUrl}")] 
+		public async Task<IActionResult> Stats(string shortenedUrl)
+		{
+			if (string.IsNullOrWhiteSpace(shortenedUrl) || !await _urlService.IsShortenedUrlValidAsync(shortenedUrl))
+			{
+				return NotFound();
+			}
+
+			var urlId = await _urlService.GetUrlIdByShortenedUrlAsync(shortenedUrl);
+
+			if (urlId == Guid.Empty)
+			{
+				return NotFound();
+			}
+
+			var userIpAddress = IpAddressHelper.GetUserIpAddress(HttpContext);
+
+			if (await _urlService.HasUrlOpenBeenRecordedTodayAsync(urlId, userIpAddress, DateTime.UtcNow) == false)
+			{
+				await _urlService.RecordUrlOpensAsync(urlId, userIpAddress);
+			}
+
+			var urlStatisticsModel = await _urlService.GetUrlStatisticsViewModelAsync(urlId);
+
+			return View(urlStatisticsModel);
+		}
+	}
+}
