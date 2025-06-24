e8e7e92394e41b1f434419f6034db84063c6bec8
karinanikolova <lianna_kn@yahoo.com>
2025-06-24 22:27:08 +0300

    Created UrlStatisticsViewModel.

---
+++ Models/Statistics/UrlStatisticsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace URLShortenerApp.Models.Statistics
+{
+	public class UrlStatisticsViewModel
+	{
+		public IEnumerable<RecordViewModel> RecordsPerUserPerDay { get; set; } = new List<RecordViewModel>();
+
+		public IEnumerable<IpVisitSummaryViewModel> Top10Users { get; set; } = new List<IpVisitSummaryViewModel>();
+	}
+}
