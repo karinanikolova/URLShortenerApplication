adffdf149263ddfa12981dd3971afa807e71c394
karinanikolova <lianna_kn@yahoo.com>
2025-06-25 20:51:12 +0300

    Created the stats.js file.

---
+++ wwwroot/js/stats.js
@@ -0,0 +1,35 @@
+﻿window.getTopVisitorsChart = function (labels, data) {
+    const ctx = document.querySelector('#topVisitorsChart').getContext('2d');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Visits Count',
+                data: data,
+                backgroundColor: 'rgba(54, 162, 235, 0.6)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    title: {
+                        display: true,
+                        text: 'Number of Visits'
+                    }
+                },
+                x: {
+                    title: {
+                        display: true,
+                        text: 'IP Address'
+                    }
+                }
+            }
+        }
+    });
+};
