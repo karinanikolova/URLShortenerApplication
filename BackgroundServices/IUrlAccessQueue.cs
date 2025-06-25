7b5fdd4ddc65d17fd255379dc00f3df427148314
karinanikolova <lianna_kn@yahoo.com>
2025-06-25 18:03:09 +0300

    Created IUrlAccessQueue for handling URL access log DTOs

---
+++ BackgroundServices/IUrlAccessQueue.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Channels;
+
+namespace URLShortenerApp.BackgroundServices
+{
+	/// <summary>
+	/// Interface for a queue that handles URL access log DTOs.
+	/// </summary>
+	public interface IUrlAccessQueue
+	{
+		ChannelReader<UrlAccessLogDto> Reader { get; }
+
+		void Enqueue(UrlAccessLogDto logDto);
+	}
+}
