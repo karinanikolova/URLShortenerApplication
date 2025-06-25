f1dea8c175bd20ecceb8276715584abb61a005e0
karinanikolova <lianna_kn@yahoo.com>
2025-06-25 18:06:07 +0300

    Created UrlAccessLogDto.

---
+++ BackgroundServices/UrlAccessLogDto.cs
@@ -0,0 +1,14 @@
+﻿namespace URLShortenerApp.BackgroundServices
+{
+	/// <summary>
+	/// Data Transfer Object for storing URL access information temporarily.
+	/// </summary>
+	public class UrlAccessLogDto
+	{
+		public Guid URLId { get; set; }
+
+		public string UserIPAddress { get; set; }
+
+		public DateTime AccessDate { get; set; }
+	}
+}
