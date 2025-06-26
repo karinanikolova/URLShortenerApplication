d3e4b25850f4b5c6243a908607571ac3797cca09
karinanikolova <lianna_kn@yahoo.com>
2025-06-26 14:45:21 +0300

    Created ICustomUrlHelper for methods for normalizing URLs.

---
+++ Helpers/Contracts/ICustomUrlHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace URLShortenerApp.Helpers.Contracts
+{
+	/// <summary>
+	/// ICustomUrlHelper interface defines methods for normalizing URLs.
+	/// </summary>
+	public interface ICustomUrlHelper
+	{
+		Task<string> Normalize(string inputUrl);
+	}
+}
