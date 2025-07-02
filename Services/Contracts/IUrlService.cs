94e1d05b935d4acd717056900dd8a17f09a58df8
karinanikolova <lianna_kn@yahoo.com>
2025-07-02 18:52:51 +0300

    Added methods, updated and renamed existing ones in IUrlService.

--- Services/Contracts/IUrlService.cs
+++ Services/Contracts/IUrlService.cs
@@ -5,20 +5,24 @@ namespace URLShortenerApp.Services.Contracts
 {
 	public interface IUrlService
 	{
-		Task AddUrlAsync(string url);
+		Task AddUrlAsync(string url, DateTime creationDate);
 
 		Task<bool> OriginalUrlExistsAsync(string url);
 
-		Task<URLViewModel> GetUrlViewModelByOriginalUrlAsync(string url);
+		Task<URLViewModel> GetUrlViewModelByUrlAndCreationDateAsync(string url, DateTime creationDate);
 
-		Task<bool> IsShortenedUrlValidAsync(string shortenedUrl);
+		Task<bool> IsSecretUrlValidAsync(string secretUrl);
 
 		Task RecordUrlOpensAsync(Guid urlId, string ipAddress, DateTime accessDate);
+
+		Task<Guid> GetUrlIdBySecretUrlAsync(string secretUrl);
 
 		Task<Guid> GetUrlIdByShortenedUrlAsync(string shortenedUrl);
 
 		Task<bool> HasUrlOpenBeenRecordedTodayAsync(Guid urlId, string ipAddress, DateTime dateTimeToday);
 
 		Task<UrlStatisticsViewModel> GetUrlStatisticsViewModelAsync(Guid urlId);
+
+		Task<string> GetOriginalUrlByShortenedUrlAsync(string shortenedUrl);
 	}
 }
