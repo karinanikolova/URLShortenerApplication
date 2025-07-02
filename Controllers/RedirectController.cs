5db0dfb73f19a9a4164cd3745e1f9a5e0b771d19
karinanikolova <lianna_kn@yahoo.com>
2025-07-02 18:41:14 +0300

    Created RedirectController for URL redirection and user access logging.

---
+++ Controllers/RedirectController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using URLShortenerApp.BackgroundServices;
+using URLShortenerApp.Helpers;
+using URLShortenerApp.Services.Contracts;
+
+namespace URLShortenerApp.Controllers
+{
+	public class RedirectController : Controller
+	{
+		private readonly IUrlService _urlService;
+		private readonly IUrlAccessQueue _accessQueue;
+
+		public RedirectController(IUrlService urlService, IUrlAccessQueue accessQueue)
+		{
+			_urlService = urlService;
+			_accessQueue = accessQueue;
+		}
+
+		[HttpGet("/{shortenedUrl}")]
+		public async Task<IActionResult> RedirectTo(string shortenedUrl)
+		{
+			var originalUrl = await _urlService.GetOriginalUrlByShortenedUrlAsync(shortenedUrl);
+
+			if (originalUrl == null)
+			{
+				return NotFound("Short URL not found.");
+			}
+
+			var urlId = await _urlService.GetUrlIdByShortenedUrlAsync(shortenedUrl);
+
+			if (urlId == Guid.Empty)
+			{
+				return NotFound();
+			}
+
+			var userIpAddress = IpAddressHelper.GetUserIpAddress(HttpContext);
+
+			if (await _urlService.HasUrlOpenBeenRecordedTodayAsync(urlId, userIpAddress, DateTime.UtcNow) == false)
+			{
+				_accessQueue.Enqueue(new UrlAccessLogDto()
+				{
+					URLId = urlId,
+					UserIPAddress = userIpAddress,
+					AccessDate = DateTime.UtcNow
+				});
+			}
+
+			return Redirect(originalUrl);
+		}
+	}
+}
