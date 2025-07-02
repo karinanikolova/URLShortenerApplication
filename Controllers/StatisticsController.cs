58352d0f171a9f7744c54da7455769ac6c81f9e6
karinanikolova <lianna_kn@yahoo.com>
2025-07-02 18:42:27 +0300

    Modified the Stats HttpGet action method in StatisticsController.

--- Controllers/StatisticsController.cs
+++ Controllers/StatisticsController.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using URLShortenerApp.BackgroundServices;
-using URLShortenerApp.Helpers;
 using URLShortenerApp.Services.Contracts;
 
 namespace URLShortenerApp.Controllers
@@ -8,39 +6,25 @@ namespace URLShortenerApp.Controllers
 	public class StatisticsController : Controller
 	{
 		private readonly IUrlService _urlService;
-		private readonly IUrlAccessQueue _accessQueue;
 
-		public StatisticsController(IUrlService urlService, IUrlAccessQueue accessQueue)
+		public StatisticsController(IUrlService urlService)
 		{
 			_urlService = urlService;
-			_accessQueue = accessQueue;
 		}
 
-		[HttpGet("/{shortenedUrl:shortUrl}")]
-		public async Task<IActionResult> Stats(string shortenedUrl)
+		[HttpGet("/{secretUrl:secretUrl}")]
+		public async Task<IActionResult> Stats(string secretUrl)
 		{
-			if (string.IsNullOrWhiteSpace(shortenedUrl) || !await _urlService.IsShortenedUrlValidAsync(shortenedUrl))
+			if (string.IsNullOrWhiteSpace(secretUrl) || !await _urlService.IsSecretUrlValidAsync(secretUrl))
 			{
 				return NotFound();
 			}
 
-			var urlId = await _urlService.GetUrlIdByShortenedUrlAsync(shortenedUrl);
+			var urlId = await _urlService.GetUrlIdBySecretUrlAsync(secretUrl);
 
 			if (urlId == Guid.Empty)
 			{
 				return NotFound();
-			}
-
-			var userIpAddress = IpAddressHelper.GetUserIpAddress(HttpContext);
-
-			if (await _urlService.HasUrlOpenBeenRecordedTodayAsync(urlId, userIpAddress, DateTime.UtcNow) == false)
-			{
-				_accessQueue.Enqueue(new UrlAccessLogDto()
-				{
-					URLId = urlId,
-					UserIPAddress = userIpAddress,
-					AccessDate = DateTime.UtcNow
-				});
 			}
 
 			var urlStatisticsModel = await _urlService.GetUrlStatisticsViewModelAsync(urlId);
