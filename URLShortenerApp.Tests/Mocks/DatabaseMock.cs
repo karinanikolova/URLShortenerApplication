50a503130b787db18607e4b0248e2d714a3eb1ff
karinanikolova <lianna_kn@yahoo.com>
2025-07-04 17:11:18 +0300

    Created DatabaseMock for creating an in-memory database.

---
+++ URLShortenerApp.Tests/Mocks/DatabaseMock.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using URLShortenerApp.Data;
+
+namespace URLShortenerApp.Tests.Mocks
+{
+	/// <summary>
+	/// Static class for database mock which provides Instance property for creating an in-memory database with a unique name without any seeded data.
+	/// </summary>
+	public static class DatabaseMock
+	{
+		public static AppDbContext Instance
+		{
+			get
+			{
+				var options = new DbContextOptionsBuilder<AppDbContext>()
+					.UseInMemoryDatabase("AppInMemoryDb" + DateTime.Now.Ticks.ToString())
+					.Options;
+
+				return new AppDbContext(options, false);
+			}
+		}
+	}
+}
