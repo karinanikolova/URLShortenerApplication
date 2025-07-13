ffaab9cca4003103d8688cee19d7e71e2b380474
karinanikolova <lianna_kn@yahoo.com>
2025-07-13 15:40:39 +0300

    Added unique indexing in RecordConfiguration.

--- Data/Configuration/RecordConfiguration.cs
+++ Data/Configuration/RecordConfiguration.cs
@@ -8,11 +8,17 @@ namespace URLShortenerApp.Data.Configuration
 	{
 		public void Configure(EntityTypeBuilder<Record> builder)
 		{
+			// Configuring the Record entity's ralationship with URL and on delete behavior
 			builder
 				.HasOne(r => r.URL)
 				.WithMany(u => u.Records)
 				.HasForeignKey(r => r.URLId)
 				.OnDelete(DeleteBehavior.Restrict);
+
+			// Configuring a composite unique index constraint which ensures that no duplicate visit records for the same user on the same day can exist
+			builder
+				.HasIndex(r => new { r.URLId, r.AccessDate, r.UserIPAddress })
+				.IsUnique();
 
 			// Seeding initial data for Records
 			builder.HasData(DataSeed.Records);
