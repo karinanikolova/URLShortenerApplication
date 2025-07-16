d64150804dd5271b6613e5bee0e53568bf323743
karinanikolova <lianna_kn@yahoo.com>
2025-07-16 17:37:37 +0300

    Created ITldProvider.

---
+++ Services/Contracts/ITldProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace URLShortenerApp.Services.Contracts
+{
+	/// <summary>
+	/// Interaface for a provider that fetches Top-Level Domains (TLDs).
+	/// </summary>
+	public interface ITldProvider
+	{
+		Task<string> GetTldsAsync();
+	}
+}
