49ed78f1b6cfe7975f65a27e7dcfaa92bce42ec1
karinanikolova <lianna_kn@yahoo.com>
2025-07-16 17:38:46 +0300

    Created IanaTldProvider for TLD retrieval.

---
+++ Services/IanaTldProvider.cs
@@ -0,0 +1,26 @@
+﻿using URLShortenerApp.Services.Contracts;
+
+namespace URLShortenerApp.Services
+{
+	/// <summary>
+	/// Provides functionality to retrieve the list of top-level domains (TLDs) from the IANA (Internet Assigned Numbers
+	/// Authority) data source.
+	/// </summary>
+	/// <remarks>This class fetches the TLD data from the official IANA TLD list, which is hosted at  <see
+	/// href="https://data.iana.org/TLD/tlds-alpha-by-domain.txt"/>. The data is returned as a plain text string, with
+	/// each TLD listed on a separate line. The caller is responsible for parsing or processing the data as
+	/// needed.</remarks>
+	public class IanaTldProvider : ITldProvider
+	{
+		private const string IanaTldUrl = "https://data.iana.org/TLD/tlds-alpha-by-domain.txt";
+
+		public async Task<string> GetTldsAsync()
+		{
+			using var httpClient = new HttpClient();
+
+			var tlds = await httpClient.GetStringAsync(IanaTldUrl);
+
+			return tlds;
+		}
+	}
+}
